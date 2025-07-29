89dba65f843f1d35b4897dff4efe1362d1e3dbbb
Roneth Hemsara <rarhemsara@gmail.com>
2025-07-29 17:06:28 +0530

    add pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./pages/header/header";
 import { Verifypage } from "./pages/verifypage/verifypage";
+import { Footer } from "./pages/footer/footer";
+import { Ourservices } from "./pages/ourservices/ourservices";
+import { Contactus } from "./pages/contactus/contactus";
+import { Signup } from "./pages/signup/signup";
+import { Login } from "./auth/login/login";
+import { Admindashboard } from "./dashboard/admindashboard/admindashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Verifypage],
+  imports: [RouterOutlet, Header, Verifypage, Footer, Ourservices, Contactus, Signup, Login, Admindashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

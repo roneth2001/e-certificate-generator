8f2f81c4d6e95cde7fe9bff34848263ae11619e7
Roneth Hemsara <rarhemsara@gmail.com>
2025-07-30 12:46:24 +0530

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component: Login},
+];
--- src/app/pages/header/header.ts
+++ src/app/pages/header/header.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from '../../auth/login/login';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Login],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
 
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    console.log('Navigating to login page...');
+    this.router.navigate(['login']);
+  }
+
 }
+
